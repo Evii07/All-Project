98a1cec7b53b378caa5f0df1d0c9b75edab8ea22
Evii07 <alfiandrachen07@gmail.com>
2026-01-22 23:03:30 +1100

    newest

--- Day_n_Night/Growing.js
+++ Day_n_Night/Growing.js
@@ -1,2 +1,16 @@
 
 
+const button = document.getElementById("button");
+
+
+button.addEventListener('click', () => {
+    const circle = document.getElementById("circle");
+    circle.classList.toggle("night_button");
+    if (circle.classList.contains("night_button")) {
+        circle.style.backgroundColor = "rgb(45, 45, 114)";
+    } else {
+        circle.style.backgroundColor = "rgb(248, 227, 3)";
+    }
+
+
+});

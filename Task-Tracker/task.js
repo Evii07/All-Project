2749c68164e7dd98acf37ad91d7753a5292b84fa
Evii07 <alfiandrachen07@gmail.com>
2026-01-22 12:15:07 +1100

    new

--- Task-Tracker/task.js
+++ Task-Tracker/task.js
@@ -1,23 +1,23 @@
 document.getElementById("form").addEventListener("submit", function(e) {
-    e.preventDefault;
+    e.preventDefault();
 
-    input = getElementById("input_text").value;
+    const input = document.getElementById("input_text").value;
     
     const new_div = document.createElement("div");
-    new_div.classList.add = "container";
+    new_div.classList.add("container");
 
     const new_label = document.createElement("label");
-    new_label.classList.add = "checkbox-label";
+    new_label.classList.add("checkbox-label");
 
     const new_input = document.createElement("input");
     new_input.type = "checkbox";
-    new_input.classList.add = "checkbox";
+    new_input.classList.add("checkbox");
 
     const span_one = document.createElement("span");
-    span_one.classList.add = "checkmark";
+    span_one.classList.add("checkmark");
 
     const span_two = document.createElement("span");
-    span_two.classList.add = "text_list";
+    span_two.classList.add("text_list");
 
     span_two.textContent = input;
 
@@ -26,5 +26,19 @@ document.getElementById("form").addEventListener("submit", function(e) {
     new_label.appendChild(span_one);
     new_label.appendChild(span_two);
 
-    document.getElementById("Body").appendChild(new_div);
+    document.getElementById("smaller_box").appendChild(new_div);
+    document.getElementById("input_text").value = "";
+});
+
+
+const created_box = document.getElementById("smaller_box");
+
+created_box.addEventListener("click", function (e) {
+    const text = e.target.closest(".text_list");
+
+    if (!text) {
+        return;
+    }
+
+    text.classList.toggle("crossed");
 })
